0a69a07ce903205774f20b66e0f30f56903402ed
Elkayam Melesse <elka@duck.com>
2022-07-17 15:49:24 +0300

    Fixed basic diamond

--- Ex01_02/Program.cs
+++ Ex01_02/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Ex01_02
 {
-    internal class Program
+    public class Program
     {
         public static void Main()
         {
-            int diamondHeight = 9;
+            int diamondHeight = 3;
             printBasicStarDiamond(diamondHeight);
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
@@ -15,41 +15,65 @@ namespace Ex01_02
         // print a diamond using asterisks with the given height.
         private static void printBasicStarDiamond(int i_Height)
         {
-            printBasicStarDiamond(i_Height, i_Height, 0);
+            printUpperTriangle(i_Height / 2, i_Height, 0);
+            printLowerTriangle(i_Height - 2, 1, i_Height / 2);
         }
 
         // recursively print a triangle.
-        private static void printBasicStarDiamond(int i_Height, int i_Width, int i_Spaces)
+        private static void printUpperTriangle(int i_Height, int i_Stars, int i_Spaces)
         {
-            if(i_Width == 1)
+            // top of the triangle:
+            if(i_Stars == 1)
             {
-                for(int j = 0; j < i_Height / 2; j++)
+                for(int j = 0; j < i_Height; j++)
                 {
                     Console.Write(' ');
                 }
 
                 Console.WriteLine('*');
             }
+
+            // rest of the triangle:
             else
             {
+                printUpperTriangle(i_Height, i_Stars - 2, i_Spaces + 1);
                 for (int j = 0; j < i_Spaces; j++)
                 {
                     Console.Write(' ');
                 }
 
-                for (int k = 0; k < i_Width; k++)
+                for (int k = 0; k < i_Stars; k++)
                 {
                     Console.Write('*');
                 }
 
                 Console.WriteLine();
-                printBasicStarDiamond(i_Height, i_Width - 2, i_Spaces + 1);
+            }
+        }
+
+        // recursively print an upside down triangle.
+        private static void printLowerTriangle(int i_Height, int i_Stars, int i_Spaces)
+        {
+            // top of the triangle:
+            if (i_Stars == i_Height)
+            {
+                Console.Write(' ');
+                for (int j = 0; j < i_Height; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine(' ');
+            }
+            else
+            {
+                printLowerTriangle(i_Height, i_Stars + 2, i_Spaces - 1);
                 for (int j = 0; j < i_Spaces; j++)
                 {
                     Console.Write(' ');
                 }
 
-                for (int k = 0; k < i_Width; k++)
+                for (int k = 0; k < i_Stars; k++)
                 {
                     Console.Write('*');
                 }
