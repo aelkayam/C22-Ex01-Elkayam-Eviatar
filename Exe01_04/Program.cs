aeb7f9c0895d8cf925a220d478227e30a8343495
evduanG <eviatar.duany@gmail.com>
2022-07-18 10:22:08 +0300

    add ex01_04 sol and enum eInputType

---
+++ Exe01_04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex01_04
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int inputStr ;
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+
+    /**
+     * A method that returns a desired input length, by type
+     */
+
+
+}
