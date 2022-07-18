d80c0d5fd52f8c23cf8e38334487259269b6f3e8
evduanG <eviatar.duany@gmail.com>
2022-07-18 18:08:19 +0300

    Unchecked code for question EX01_04 EX01_05 EX01_01 needs to be arranged in order to finish

--- eInputType/Program.cs
+++ eInputType/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum eInputType
+{
+    Numbers,
+    Binary,
+    Letters,
+    LettersAndNumbers
+}
